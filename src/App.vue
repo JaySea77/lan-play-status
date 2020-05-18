4d79065b20944569ed6f5b927ac94bd83d1c36d9
JaySea77 <jaysea@jayseateam.nl>
2020-05-18 22:42:48 +0200

    Updated to reflect original

--- src/App.vue
+++ src/App.vue
@@ -36,12 +36,9 @@ a {
 
 .icon {
   width: 22px;
-  &--spin {
-    width: 12px;
-    animation-name: spin;
-    animation-duration: 1500ms;
-    animation-iteration-count: infinite;
-    animation-timing-function: linear;
+
+  &--small {
+    width: 16px;
   }
 }
 
---
+++ src/components/rooms/01006f8002326000.vue
@@ -0,0 +1,21 @@
+<template>
+  <span>
+    <img
+      :src="require(`@/assets/tom-nook.png`)"
+      class="icon icon--small"
+      alt=""
+    />
+
+    <strong> {{ this.room.hostPlayerName }}</strong> 🏝️
+    <strong>{{ this.advertiseData.island }}</strong>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    room: Object,
+    advertiseData: Object
+  }
+};
+</script>
